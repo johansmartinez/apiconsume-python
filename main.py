b84ee85c50cdb7b60a86a1bcb3f400ea0df2aa87
johansmartinez <johan.martinez.dev@gmail.com>
2022-03-04 14:41:51 -0500

    Agregando consumo a mútiples años

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import json
 import metricsByYear
 import json
 
-D14=metricsByYear(2014, 'https://opendata.arcgis.com/datasets/505e89d2ade143a684d51b60236ba285_0.geojson')
 
-f = open("{}.json".format(2014), "w")
-f.write(json.dumps(D14, ensure_ascii=False))
-f.close()
+def writeFileData():
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+        for element in config:
+            data=metricsByYear(element['year'], element['url'])
+            f = open("dataset/{}.json".format(element['year']), "w")
+            f.write(json.dumps(data, ensure_ascii=False))
+            f.close()
+            
+writeFileData()
